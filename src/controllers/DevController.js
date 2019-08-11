e0de9678ab78bf096b78457c5f11a42f23caa55d
Cristiano Castro <cristianodmtsb@gmail.com>
2019-08-11 14:53:17 -0300

    Socket Io install

--- src/controllers/DevController.js
+++ src/controllers/DevController.js
@@ -24,7 +24,7 @@ module.exports = {
     const userExists = await Dev.findOne({ user: username });
 
     if (userExists) {
-      return res.json();
+      return res.json(userExists);
     }
 
     const response = await axios.get(
